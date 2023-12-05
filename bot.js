0682965aa3b8a355b4bc67116b89dcd1e9d9457b
Sandun Mihiranga <91211241+SandunMihiranga2004@users.noreply.github.com>
2023-12-05 16:26:34 +0530

    © ꜱᴀɴᴅᴜɴ ᴍɪʜɪʀᴀɴɢᴀ

--- bot.js
+++ bot.js
@@ -1251,7 +1251,7 @@ case "xnxxdl": {
         if (!text.includes('xnxx.com')) return m.reply(`Enter an xnxx link`)
         const fg = require('api-dylux')
             let xn = await fg.xnxxdl(text)
-gss.sendMessage(m.chat, { caption: `  *XNXX DL*
+client.sendMessage(m.chat, { caption: `  *XNXX DL*
         
 ✍ *Title:* ${xn.title}
 ⌛ *Duration:* ${xn.duration}
@@ -1260,7 +1260,7 @@ gss.sendMessage(m.chat, { caption: `  *XNXX DL*
 break
 
 
-case 'xnxxsearch': {
+case 'xnxxdh': {
 	if (!text) return m.reply(`Enter Query`)
 	const fg = require('api-dylux')
 	let res = await fg.xnxxSearch(text)
